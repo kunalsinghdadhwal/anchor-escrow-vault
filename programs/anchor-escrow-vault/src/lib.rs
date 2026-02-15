a8b0d46d059cbee486d554bcfd863dcdb03aa52c
Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>
2026-02-15 20:12:15 +0530

    lib file
    Signed-off-by: Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>

--- programs/anchor-escrow-vault/src/lib.rs
+++ programs/anchor-escrow-vault/src/lib.rs
@@ -9,13 +9,22 @@ pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("8tzbBW86ymqXnHAN5H1WxQTErjEzvezgtFtCzQvM9hVA");
+declare_id!("5UFZzEt5vU9fxtUAgsD11z63ApZEHJ5bH7Z4QpFwZ2CQ");
 
 #[program]
 pub mod anchor_escrow_vault {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.deposit(deposit)?;
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)
     }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+
+    // pub fn take(ctx: Context<Take>) -> Result<()> {
+    //     ctx.accounts.deposit()?;
+    //     ctx.accounts.withdraw_and_close_vault()
+    // }
 }
